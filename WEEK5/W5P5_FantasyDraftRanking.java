376210b2f04c52b4b8b70d330bca465c137732a7
Deepika Reddy <deepika.weeb@gmail.com>
2026-09-19 23:11:45 +0530

    Add Week 5 array and OOP problems

---
+++ WEEK5/W5P5_FantasyDraftRanking.java
@@ -0,0 +1,133 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class W5P5_FantasyDraftRanking {
+
+    static class Player implements Comparable<Player> {
+
+        private String name;
+        private int matchesPlayed;
+        private double battingAverage;
+        private boolean injured;
+
+        public Player(String name, int matchesPlayed,
+                      double battingAverage, boolean injured) {
+
+            this.name = name;
+            this.matchesPlayed = matchesPlayed;
+            this.battingAverage = battingAverage;
+            this.injured = injured;
+        }
+
+        static boolean isDraftable(int matchesPlayed) {
+            return matchesPlayed >= 10;
+        }
+
+        static boolean isDraftable(int matchesPlayed, boolean injured) {
+            return matchesPlayed >= 5 && !injured;
+        }
+
+        @Override
+        public int compareTo(Player other) {
+
+            return Double.compare(
+                other.battingAverage,
+                this.battingAverage
+            );
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        boolean isDraftablePlayer() {
+
+            return isDraftable(matchesPlayed)
+                || isDraftable(matchesPlayed, injured);
+        }
+    }
+
+    static String draftAndRank(Player[] players) {
+
+        int count = 0;
+
+        for (Player player : players) {
+
+            if (player.isDraftablePlayer()) {
+                count++;
+            }
+        }
+
+        Player[] draftable = new Player[count];
+
+        int index = 0;
+
+        for (Player player : players) {
+
+            if (player.isDraftablePlayer()) {
+                draftable[index] = player;
+                index++;
+            }
+        }
+
+        Arrays.sort(draftable);
+
+        String result = "";
+
+        for (int i = 0; i < draftable.length; i++) {
+
+            result += (i + 1) + ". " + draftable[i].getName();
+
+            if (i < draftable.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of players: ");
+        int n = sc.nextInt();
+
+        sc.nextLine();
+
+        Player[] players = new Player[n];
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("\nPlayer " + (i + 1));
+
+            System.out.print("Enter name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter matches played: ");
+            int matchesPlayed = sc.nextInt();
+
+            System.out.print("Enter batting average: ");
+            double battingAverage = sc.nextDouble();
+
+            System.out.print("Is the player injured? (true/false): ");
+            boolean injured = sc.nextBoolean();
+
+            sc.nextLine();
+
+            players[i] = new Player(
+                name,
+                matchesPlayed,
+                battingAverage,
+                injured
+            );
+        }
+
+        String result = draftAndRank(players);
+
+        System.out.println("\nDraft Ranking:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}   
